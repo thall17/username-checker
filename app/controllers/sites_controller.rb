17d14ae531d9ed64f1101d25cec7a0256126f512
Tim Hall <thall74@gatech.edu>
2017-08-17 00:20:44 -0700

    Add linkedin uri

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,8 +4,16 @@ require "uri"
 class SitesController < ApplicationController
 
   def index
-    @github_uri = uri = URI.parse("https://github.com/timjhall")
-    @github_response = Net::HTTP.get_response(uri)
+    username = "timjhall"
+
+    #URI's
+    # @github_uri = uri = URI.parse("https://github.com/#{username}")
+    # @linkedin_uri = uri = URI.parse("https://www.linkedin.com/in/#{username}")
+
+    #Responses
+    @github_response = Net::HTTP.get_response(URI.parse("https://github.com/#{username}"))
+    @linkedin_response = Net::HTTP.get_response(URI.parse("https://www.linkedin.com/in/#{username}"))
+
   end
 
 end
