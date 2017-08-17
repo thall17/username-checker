a029118d1cbda38d1432ec074a52f9a390cda488
Tim Hall <thall74@gatech.edu>
2017-08-17 00:10:27 -0700

    Add URI github check

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,11 @@
-require 'net/http'
+require "net/http"
+require "uri"
+
 class SitesController < ApplicationController
 
   def index
+    @github_uri = uri = URI.parse("https://github.com/timjhall")
+    @github_response = Net::HTTP.get_response(uri)
   end
+
 end
