7e289b64321a202b18684c57e9fe46dd1ed79959
Tim Hall <thall74@gatech.edu>
2017-08-17 22:04:10 -0700

    Add form to index action

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,21 +4,22 @@ require "uri"
 class SitesController < ApplicationController
 
   def index
-    username = "timjhall"
+    # username = "timjhall"
 
     #URI's
     # @github_uri = uri = URI.parse("https://github.com/#{username}")
     # @linkedin_uri = uri = URI.parse("https://www.linkedin.com/in/#{username}")
 
     #Responses
-    @github_response = Net::HTTP.get_response(URI.parse("https://github.com/#{username}"))
-    @linkedin_response = Net::HTTP.get_response(URI.parse("https://www.linkedin.com/in/#{username}"))
-    @twitter_response = Net::HTTP.get_response(URI.parse("https://twitter.com/#{username}"))
-    @instagram_response = Net::HTTP.get_response(URI.parse("https://www.instagram.com/#{username}/"))
+    @github_response = Net::HTTP.get_response(URI.parse("https://github.com/#{@username}"))
+    @linkedin_response = Net::HTTP.get_response(URI.parse("https://www.linkedin.com/in/#{@username}"))
+    @twitter_response = Net::HTTP.get_response(URI.parse("https://twitter.com/#{@username}"))
+    @instagram_response = Net::HTTP.get_response(URI.parse("https://www.instagram.com/#{@username}/"))
+  end
 
-    
-
-
+  def form
+    @username = "timjhall"
+    render :action => :index
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/sites/index', to: 'sites#index', as: 'index'
+
   root 'sites#index'
+
+
+  post '/sites/test', to: 'sites#test', as: 'test'
+
+
 end
