3dd43e222b70036e6840fdf8bf7848de61558633
Tim Hall <thall74@gatech.edu>
2017-10-02 22:53:47 -0700

    Instagram check

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -26,12 +26,12 @@ class SitesController < ApplicationController
     # If form field is not blank, validate it per each site's rules to populate each row in the table.
     else
       @results = { 
-        GitHub: {icon: "", result: check_github(@name)},
-        LinkedIn: {ricon: "", esult: check_linkedin(@name)},
-        Twitter: {ricon: "", esult: check_twitter(@name)},
-        Instagram: {icon: "", result: check_instagram(@name)},
-        Facebook: {icon: "", result: check_facebook(@name)},
-        Bitbucket: {icon: "", result: check_bitbucket(@name)}
+        GitHub: {icon: "fa-github", result: check_github(@name)},
+        LinkedIn: {icon: "fa-linkedin", esult: check_linkedin(@name)},
+        Twitter: {icon: "fa-twitter", esult: check_twitter(@name)},
+        Instagram: {icon: "fa-instagram", result: check_instagram(@name)},
+        Facebook: {icon: "fa-facebook", result: check_facebook(@name)},
+        Bitbucket: {icon: "fa-bitbucket", result: check_bitbucket(@name)}
       }
     end
 
@@ -99,8 +99,6 @@ class SitesController < ApplicationController
       too_shoort_or_long = (name.length < 5 or name.length > 30) # Must be a certain length.
 
       
-
-
       if too_shoort_or_long
         result << "Wrong format."
         if too_shoort_or_long
@@ -117,24 +115,17 @@ class SitesController < ApplicationController
     end
 
     def check_twitter(name)
+      # Letters, numbers, underscores only.
+      # Max length: 15 characters
+
       result = ""
-      # if name violates any of the rules, add "Wrong format..."
 
-      # Letters, numbers, and underscores only.
-      # if anything else...
-
-      # My note: do underscores get converted to something else in URL?
-
-      # There is apparently no minimum-length requirement; the user a exists on Twitter. Maximum length is 15 characters.
-      # if name.length > 15
-      #   result << "Username must be less than 16 characters long."
-      # end
-      # There is also no requirement that the name contain letters at all; the user 69 exists, as does a user whose name I can’t pronounce.
+      # Check if length is okay
       too_shoort_or_long = (name.length > 15) # Must be a certain length.
 
+      # Check if alphanumeric/undercores only
       anu_regex = /^[a-zA-Z0-9_]*$/
       regex_result = anu_regex =~ name
-
       if (regex_result.is_a? Integer) and (regex_result != 0)
         print("regex_result = #{regex_result}")
         non_alphanum_or_underscore = false
@@ -142,6 +133,7 @@ class SitesController < ApplicationController
         non_alphanum_or_underscore = true
       end
 
+      # Create result string based on rule checks
       if too_shoort_or_long or non_alphanum_or_underscore
         result << "Wrong format."
         if too_shoort_or_long
@@ -151,6 +143,7 @@ class SitesController < ApplicationController
           result << " Must only contain alphanumeric characters or underscores."
         end
       else
+        response = Net::HTTP.get_response(URI.parse("https://twitter.com/#{name}"))
         if response.code == '200'
           result = "Username taken"
         else
@@ -161,19 +154,40 @@ class SitesController < ApplicationController
     end
 
     def check_instagram(name)
+      # Letters, numbers, periods, underscores only
+      # Max length is 30
       result = ""
-      # if name violates any of the rules, add "Wrong format..."
 
-      # Limit - 30 chars. Username must contains only letters, numbers, periods and underscores.
-      # If > 30 chars...
-      # if name.length > 30
-      #   result << "Max length = 30 characters."
-      # end
+      # Check if length is okay
+      too_shoort_or_long = (name.length > 30) # Must be a certain length.
 
-      # if contains anytihng else besides letters, numbers, periods, underscores...
+      # Check if alphanumeric/undercores only
+      anu_regex = /^[a-zA-Z0-9_]*$/
+      regex_result = anu_regex =~ name
+      if (regex_result.is_a? Integer) and (regex_result != 0)
+        print("regex_result = #{regex_result}")
+        non_alphanum_or_underscore = false
+      else
+        non_alphanum_or_underscore = true
+      end
 
-      response = Net::HTTP.get_response(URI.parse("https://www.instagram.com/#{name}/"))
-      result = response
+      # Create result string based on rule checks
+      if too_shoort_or_long or non_alphanum_or_underscore
+        result << "Wrong format."
+        if too_shoort_or_long
+          result << " Must be less than 16 characters."
+        end
+        if non_alphanum_or_underscore
+          result << " Must only contain alphanumeric characters or underscores."
+        end
+      else
+        response = Net::HTTP.get_response(URI.parse("https://instagram.com/#{name}"))
+        if response.code == '200'
+          result = "Username taken"
+        else
+          result = "Available"
+        end
+      end
       return result
     end
   
